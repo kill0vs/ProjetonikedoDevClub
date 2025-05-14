895f490531cdcd5f1f64128974c57de8bf2be7b6
kill0vs <viana.lucas5556@gmail.com>
2025-05-13 21:29:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+// document = html
+// querySelector = pegador / selecionador
+
+function mudarvisual(cor, imagem) {
+    tenis.classList.add("troca-efeito")
+
+    body.style.background = cor
+    
+    // contador de tempo
+    setTimeout(() => {
+        tenis.src = imagem
+        tenis.classList.remove("troca-efeito")
+    }, 500 );
+    //1000 milisegundos = 1 segundo
+}
